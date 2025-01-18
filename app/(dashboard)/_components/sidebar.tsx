9f013d32bbee1bea285640d016bd55aa353e2fbc
Keshav Kumar <aboutkeshavkumar@hotmail.com>
2025-01-18 23:42:39 +0530

    add layout and components to dashboard

---
+++ app/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,13 @@
+import Logo from "./logo";
+
+const SideBar = () => {
+   return (
+      <div className="h-full border-r flex-col overflow-y-auto bg-white shadow-sm ">
+         <div className="p-6">
+            <Logo />
+         </div>
+      </div>
+   );
+}
+
+export default SideBar;
